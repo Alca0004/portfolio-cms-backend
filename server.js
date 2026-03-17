6912524d008edd879be0aa9a5d29325f57298348
Aaron Alcala <alca0004@gmail.com>
2026-03-17 16:24:53 -0700

    connect backend to MongoDB with mongoose

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+require("dotenv").config();
+
+const connectDB = require("./src/config/db");
 
 const app = express();
 
-const PORT = 3000;
+connectDB();
+
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("API running");
