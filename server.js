7980d2549a3aac9a86f02e679a464af8b7ae28c0
Aaron Alcala <alca0004@gmail.com>
2026-04-12 09:25:51 -0700

    add auth middleware with JWT protection

--- server.js
+++ server.js
@@ -3,20 +3,25 @@ require("dotenv").config();
 
 const connectDB = require("./src/config/db");
 const authRoutes = require("./src/routes/authRoutes");
+const { protect } = require("./src/middleware/authMiddleware");
 
 const app = express();
 
 connectDB();
 
 app.use(express.json());
-
 app.use("/api/auth", authRoutes);
-
-const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("API running");
 });
+
+// Temporary test route 👇
+app.get("/api/test-protected", protect, (req, res) => {
+  res.json({ message: "You are authorized!", user: req.user });
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
